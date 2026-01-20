95e420e1f40607c52070f3ba30693a181d1f7d01
BestBuy <akkipaypal10@gmail.com>
2026-01-20 14:46:18 -0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-center h-16">
           <img 
-            src="/zelle-logo.png" 
+            src="/venmo-logo.png" 
             alt="Zelle" 
             className="h-10 w-auto"
           />
